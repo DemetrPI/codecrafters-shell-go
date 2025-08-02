9d351f41d756d6b62aed03ff8645f1ee88f68347
demetrPI <demetriy.78@gmail.com>
2025-08-02 12:29:30 +0200

    commit

--- app/main.go
+++ app/main.go
@@ -7,6 +7,38 @@ import (
 	"strings"
 )
 
+type command struct {
+	name        string
+	description string
+}
+type commands []command
+
+var cmds = commands{
+	
+		{
+			name:        "echo",
+			description: "is a shell builtin",
+		},
+		{
+			name:        "type",
+			description: "is a shell builtin",
+		},
+		{
+			name:        "exit",
+			description: "is a shell builtin",
+		},
+	
+}
+
+func getCommandDescription(name string) string {
+	for _, cmd := range cmds {
+		if cmd.name == name {
+			return cmd.description
+		}
+	}
+	return ""
+}
+
 func main() {
 
 	for {
@@ -23,6 +55,14 @@ func main() {
 			switch subcommands[0] {
 			case "echo":
 				fmt.Printf("%s", strings.Join(subcommands[1:], " "))
+			case "type":
+				subcommandName := strings.TrimSpace(subcommands[1])
+				commandDescription := getCommandDescription(subcommandName)
+				if commandDescription != "" {
+					fmt.Printf("%s %s\n", subcommandName, commandDescription)
+				} else {
+					fmt.Printf("%s: command not found\n", subcommandName)
+				}
 			default:
 				fmt.Println(command[:len(command)-1] + ": command not found")
 			}
