1ce268494de95543579b89303f62bbfe41bdcd08
demetrPI <demetriy.78@gmail.com>
2025-08-02 16:17:12 +0200

    codecrafters submit [skip ci]

--- app/main.go
+++ app/main.go
@@ -5,9 +5,9 @@ import (
 	"fmt"
 	"os"
 	"os/exec"
+	"path/filepath"
 	"strings"
 )
-
 
 var cmdsMap = map[string]string{
 	"echo": "a shell builtin",
@@ -15,34 +15,73 @@ var cmdsMap = map[string]string{
 	"exit": "a shell builtin",
 }
 
+func findExecutable(command string, paths []string) string {
+	for _, path := range paths {
+		fullPath := filepath.Join(path, command)
+		fileInfo, err := os.Stat(fullPath)
+		if err == nil && fileInfo.Mode().Perm()&0111 != 0 {
+			return fullPath
+		}
+	}
+	return ""
+}
 
 func main() {
-
 	for {
+		paths := strings.Split(os.Getenv("PATH"), ":")
 		fmt.Fprint(os.Stdout, "$ ")
-		command, err := bufio.NewReader(os.Stdin).ReadString('\n')
+		line, err := bufio.NewReader(os.Stdin).ReadString('\n')
 		if err != nil {
 			fmt.Fprintln(os.Stderr, "Error reading input:", err)
 			os.Exit(1)
 		}
-		if command == "exit 0\n" {
+
+		line = strings.TrimSpace(line)
+		if line == "" {
+			continue
+		}
+
+		parts := strings.Fields(line) 
+		command := parts[0]
+		args := parts[1:]
+		if command == "exit" && len(args) > 0 && args[0] == "0" {
 			os.Exit(0)
-		} else {
-			subcommands := strings.Split(command, " ")
-			switch subcommands[0] {
-			case "echo":
-				fmt.Printf("%s", strings.Join(subcommands[1:], " "))
-			case "type":
-				subcommandName := strings.TrimSpace(subcommands[1])
-				if desc, ok := cmdsMap[subcommandName]; ok {
-					fmt.Printf("%s is %s\n", subcommandName, desc)
-				} else if path, err := exec.LookPath(subcommandName); err == nil {
-					fmt.Printf("%s is %s\n", subcommandName, path)
+		}
+
+		switch command {
+		case "echo":
+			fmt.Println(strings.Join(args, " "))
+
+		case "type":
+			if len(args) > 0 {
+				target := args[0]
+				if decs, ok := cmdsMap[target]; ok {
+					fmt.Printf("%s is %s\n", target, decs)
 				} else {
-					fmt.Printf("%s: not found\n", subcommandName)
+					filePath := findExecutable(target, paths)
+					if filePath != "" {
+						fmt.Printf("%s is %s\n", target, filePath)
+					} else {
+						fmt.Printf("%s: not found\n", target)
+					}
 				}
-			default:
-				fmt.Println(command[:len(command)-1] + ": command not found")
+			} else {
+				fmt.Println("type: not enough arguments")
+			}
+
+		default:
+			filePath := findExecutable(command, paths)
+			if filePath != "" {
+				cmd := exec.Command(command, args...)
+				cmd.Stdout = os.Stdout
+				cmd.Stderr = os.Stderr
+				cmd.Stdin = os.Stdin
+				err := cmd.Run()
+				if err != nil {
+					fmt.Fprintf(os.Stderr, "Error running command: %v\n", err)
+				}
+			} else {
+				fmt.Println(command + ": command not found")
 			}
 		}
 	}
